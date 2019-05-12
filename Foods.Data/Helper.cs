4f890e86f396e9365d9aee9082b6ba4d66b97a1a
Fatih YILDIZHAN <fatih@momencraft.com>
2019-05-12 16:33:40 +0100

    begin connection

---
+++ Foods.Data/Helper.cs
@@ -0,0 +1,12 @@
+﻿using System.Configuration;
+
+namespace Foods.Data
+{
+    public static class Helper
+    {
+        public static string ConValue(string name)
+        {
+            return ConfigurationManager.ConnectionStrings[name].ConnectionString;
+        }
+    }
+}
--- Foods.UI/Main.Designer.cs
+++ Foods.UI/Main.Designer.cs
@@ -28,13 +28,35 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.btnAddCountry = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
             this.SuspendLayout();
+            // 
+            // btnAddCountry
+            // 
+            this.btnAddCountry.Location = new System.Drawing.Point(12, 12);
+            this.btnAddCountry.Name = "btnAddCountry";
+            this.btnAddCountry.Size = new System.Drawing.Size(144, 56);
+            this.btnAddCountry.TabIndex = 0;
+            this.btnAddCountry.Text = "button1";
+            this.btnAddCountry.UseVisualStyleBackColor = true;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 32;
+            this.listBox1.Location = new System.Drawing.Point(170, 268);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(300, 228);
+            this.listBox1.TabIndex = 1;
             // 
             // Main
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(16F, 32F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1372, 902);
+            this.ClientSize = new System.Drawing.Size(709, 567);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.btnAddCountry);
             this.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
             this.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
             this.Name = "Main";
@@ -44,5 +66,8 @@
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnAddCountry;
+        private System.Windows.Forms.ListBox listBox1;
     }
 }
